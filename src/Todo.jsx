196d7918fb5d0830f06d2c5e1b432636b74b544f
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-06-02 11:11:18 -0400

    diferent ways to render with conditions

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -15,11 +15,48 @@
 //     }
 // }
 
-export default function ToDo({task, isDone, time = 0}){
+// export default function ToDo({task, isDone, time = 0}){
+//     if(isDone === true){
+//         return <li>Done: {task} Duration: {time}</li>
+//     }
+//     else{
+//         return <li>To be done: {task}</li>
+//     }
+// }
+
+// conditional rendering: 3 ternary
+// condition ? true : false
+
+// export default function ToDo({task, isDone, time = 0}){
+//     return isDone ?
+//      <li>Done: {task} Time: {time}</li> 
+//      : <li>Not Done: {task}</li> 
+// }
+
+// // conditional rendering : 4 &&
+
+// export default function ToDo({task, isDone, time = 0}){
+//     return isDone && <li>Done Task: {task} time: {time}</li>
+// }
+
+
+// conditional rendering : 5 ||
+
+// export default function ToDo({task, isDone, time = 0}){
+//     return isDone || <li>Not Done Task: {task} time: {time}</li>
+// }
+
+// conditional rendering: 6 use variable
+
+export default function ToDo({task, isDone}){
+    let listItem;
+
     if(isDone === true){
-        return <li>Done: {task} Duration: {time}</li>
+        listItem = <li>Done: {task}</li>
     }
     else{
-        return <li>To be done: {task}</li>
+        listItem = <li>Pending: {task}</li>
     }
+
+    return listItem
 }
